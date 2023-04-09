d67084d34aad2fed08e6bd6609358f9ed023a53d
toadless <me@toadless.net>
2023-04-09 09:33:16 +0100

    add header interactivity

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -24,36 +24,45 @@ export default function Header() {
         <HeaderContainer className="w-screen h-24 border-b-[1px] border-gray-900 text-gray-600">
           <HeaderItem items={3} className="justify-center">
             {windowSize.width! >= 650 ?
-              <img src="/radio.png" width="200" />
+              <img src="/radio.png" width="200" className="cursor-pointer" onClick={() => window.location.href = window.location.origin} />
               :
-              <img src="/r.png" width="60" />
+              <img src="/r.png" width="60" className="cursor-pointer" onClick={() => window.location.href = window.location.origin} />
             }
           </HeaderItem>
           <HeaderItem items={3} className="justify-center border-l-[1px] border-gray-900">
             <div className={"flex" + (windowSize.width! >= 600 ? "" : "flex-col text-center")}>
-              <HeaderText className="p-2">HOME</HeaderText>
-              <HeaderText className="p-2">SERVERS</HeaderText>
+              <HeaderText className="p-2" url={window.location.origin}>HOME</HeaderText>
+              <HeaderText className="p-2" url={`${window.location.origin}/guilds`}>SERVERS</HeaderText>
             </div>
           </HeaderItem>
           <HeaderItem items={3} className="justify-between">
             {windowSize.width! >= 600 ?
               <HeaderItem items={2} className="justify-center p-5 border-l-[1px] border-gray-900">
-                <FontAwesomeIcon icon={faGithubSquare} className="p-1 cursor-pointer text-white hover:text-violet-400" />
-                <FontAwesomeIcon icon={faDiscord} className="p-1 cursor-pointer text-white hover:text-violet-400" />
-                <FontAwesomeIcon icon={faGlobe} className="p-1 cursor-pointer text-white hover:text-violet-400" />
+                <FontAwesomeIcon icon={faGithubSquare} className="p-1 cursor-pointer text-white hover:text-violet-400" onClick={() => {
+                  window.location.href = "https://github.com/toadless/radio"
+                }} />
+                <FontAwesomeIcon icon={faDiscord} className="p-1 cursor-pointer text-white hover:text-violet-400" onClick={() => {
+                  window.location.href = "https://radio-api.toadless.net/invite"
+                }} />
+                <FontAwesomeIcon icon={faGlobe} className="p-1 cursor-pointer text-white hover:text-violet-400" onClick={() => {
+                  window.location.href = "https://toadless.net"
+                }} />
               </HeaderItem>
               : null}
 
             <HeaderItem items={2} className="justify-center border-l-[1px] w-[100%] border-gray-900">
               {user == null ?
-                <HeaderText>LOGIN</HeaderText>
+                <HeaderText url={`${process.env.NEXT_PUBLIC_API_URL}/auth/authorize`}>LOGIN</HeaderText>
                 :
                 <HeaderItem items={1} className="justify-between">
                   <img src={user.avatar} width={60} className="rounded-full cursor-pointer p-2" onClick={() => {
                     window.location.href = window.location.origin + "/guilds"
                   }} />
 
-                  <FontAwesomeIcon icon={faRightFromBracket} className="p-1 cursor-pointer text-gray-600 hover:text-violet-400" />
+                  <FontAwesomeIcon icon={faRightFromBracket} className="p-1 cursor-pointer text-gray-600 hover:text-violet-400" onClick={() => {
+                    localStorage.clear();
+                    window.location.href = window.location.origin
+                  }} />
                 </HeaderItem>
               }
             </HeaderItem>
--- components/header/HeaderText.tsx
+++ components/header/HeaderText.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
-export default function HeaderText(props: { className?: string, children: ReactNode }) {
-    return <p className={"cursor-pointer text-gray-600 hover:text-violet-400 " + props.className}>{props.children}</p>
+export default function HeaderText(props: { className?: string, url?: string, children: ReactNode }) {
+    return <p className={"cursor-pointer text-gray-600 hover:text-violet-400 " + props.className} onClick={() => {
+        window.location.href = props.url || window.location.href
+    }}>{props.children}</p>
 }
